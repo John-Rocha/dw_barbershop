956e2a687babfb375a8b92068af33068e0a9d090
John Rocha <jrocha@tractian.com>
2023-08-18 22:31:33 -0300

    configura a splash page

--- lib/app/features/splash/splash_page.dart
+++ lib/app/features/splash/splash_page.dart
@@ -1,15 +1,60 @@
 import 'package:flutter/material.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  var _scale = 10.0;
+  var _animationOpacity = 0.0;
+
+  double get _logoAnimationWidth => 100 * _scale;
+  double get _logoAnimationHeight => 120 * _scale;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setState(() {
+        _animationOpacity = 1.0;
+        _scale = 1.0;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Splash'),
+      backgroundColor: Colors.black,
+      body: DecoratedBox(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background_image_chair.jpg'),
+            fit: BoxFit.cover,
+            opacity: 0.2,
+          ),
+        ),
+        child: Center(
+          child: AnimatedOpacity(
+            duration: const Duration(seconds: 3),
+            curve: Curves.easeIn,
+            opacity: _animationOpacity,
+            child: AnimatedContainer(
+              width: _logoAnimationWidth,
+              height: _logoAnimationHeight,
+              duration: const Duration(seconds: 3),
+              curve: Curves.linearToEaseOut,
+              child: Image.asset(
+                'assets/images/imgLogo.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ),
       ),
-      body: Container(),
     );
   }
 }
