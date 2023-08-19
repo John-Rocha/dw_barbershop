50bb22ef399ec18b9518d4ad6c4cd2909d606ebc
John Rocha <jrocha@tractian.com>
2023-08-18 23:28:15 -0300

    implementa o tema do app

--- lib/app/barber_shop_app.dart
+++ lib/app/barber_shop_app.dart
@@ -1,4 +1,5 @@
 import 'package:asyncstate/widget/async_state_builder.dart';
+import 'package:dw_barbershop/app/core/ui/barber_shop_theme.dart';
 import 'package:dw_barbershop/app/core/ui/widgets/barber_shop_loader.dart';
 import 'package:dw_barbershop/app/features/auth/login/login_page.dart';
 import 'package:dw_barbershop/app/features/splash/splash_page.dart';
@@ -16,6 +17,7 @@ class BarberShopApp extends StatelessWidget {
           title: 'DW BarberShop',
           debugShowCheckedModeBanner: false,
           navigatorObservers: [asyncNavigatorObserver],
+          theme: BarberShopTheme.themeData,
           initialRoute: '/',
           routes: {
             '/': (_) => const SplashPage(),
