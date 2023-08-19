cf65720a6338f6219ec66dfe7babd9a783133ca2
John Rocha <jrocha@tractian.com>
2023-08-18 22:04:36 -0300

    configura o loader

--- lib/app/barber_shop_app.dart
+++ lib/app/barber_shop_app.dart
@@ -1,4 +1,5 @@
 import 'package:asyncstate/widget/async_state_builder.dart';
+import 'package:dw_barbershop/app/core/ui/widgets/barber_shop_loader.dart';
 import 'package:dw_barbershop/app/features/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
@@ -8,6 +9,7 @@ class BarberShopApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AsyncStateBuilder(
+      customLoader: const BarberShopLoader(),
       builder: (asyncNavigatorObserver) {
         return MaterialApp(
           title: 'DW BarberShop',
