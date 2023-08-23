68ed8635bc4a2c6cfc16a6a94d08ba23896949b9
JohnRocha <jrocha@tractian.com>
2023-08-23 20:53:34 -0300

    Criado o UserLoginServiceProvider

--- lib/app/core/providers/application_providers.dart
+++ lib/app/core/providers/application_providers.dart
@@ -1,6 +1,8 @@
 import 'package:dw_barbershop/app/core/rest_client/rest_client.dart';
 import 'package:dw_barbershop/app/repositories/user/user_repository.dart';
 import 'package:dw_barbershop/app/repositories/user/user_repository_impl.dart';
+import 'package:dw_barbershop/app/services/user_login/user_login_service.dart';
+import 'package:dw_barbershop/app/services/user_login/user_login_service_impl.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'application_providers.g.dart';
@@ -11,3 +13,7 @@ RestClient restClient(RestClientRef rest) => RestClient();
 @Riverpod(keepAlive: true)
 UserRepository userRepository(UserRepositoryRef ref) =>
     UserRepositoryImpl(restClient: ref.read(restClientProvider));
+
+@Riverpod(keepAlive: true)
+UserLoginService userLoginService(UserLoginServiceRef ref) =>
+    UserLoginServiceImpl(userRepository: ref.read(userRepositoryProvider));
