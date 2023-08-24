0881df708982c74c8949a514a3c53e72e536a2b2
JohnRocha <jrocha@tractian.com>
2023-08-23 21:48:39 -0300

    Criado o view model e integrado com o service

--- lib/app/features/auth/login/login_page.dart
+++ lib/app/features/auth/login/login_page.dart
@@ -1,16 +1,20 @@
 import 'package:dw_barbershop/app/core/ui/constants.dart';
 import 'package:dw_barbershop/app/core/ui/helpers/form_helper.dart';
+import 'package:dw_barbershop/app/core/ui/helpers/messages.dart';
+import 'package:dw_barbershop/app/features/auth/login/login_state.dart';
+import 'package:dw_barbershop/app/features/auth/login/login_vm.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:validatorless/validatorless.dart';
 
-class LoginPage extends StatefulWidget {
+class LoginPage extends ConsumerStatefulWidget {
   const LoginPage({super.key});
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  ConsumerState<LoginPage> createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _LoginPageState extends ConsumerState<LoginPage> {
   final _formKey = GlobalKey<FormState>();
   final _emailEC = TextEditingController();
   final _passwordEC = TextEditingController();
@@ -24,6 +28,32 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
+    final LoginVm(:login) = ref.watch(loginVmProvider.notifier);
+
+    ref.listen(
+      loginVmProvider,
+      (_, state) {
+        switch (state) {
+          case LoginState(status: LoginStateStatus.initial):
+            break;
+          case LoginState(status: LoginStateStatus.error, :final errorMessage?):
+            Messages.showError(
+              errorMessage,
+              context,
+            );
+          case LoginState(status: LoginStateStatus.error):
+            Messages.showError(
+              'Erro ao realizar login',
+              context,
+            );
+          case LoginState(status: LoginStateStatus.admLogin):
+            break;
+          case LoginState(status: LoginStateStatus.employeeLogin):
+            break;
+        }
+      },
+    );
+
     return Scaffold(
       backgroundColor: Colors.black,
       body: Form(
@@ -107,7 +137,17 @@ class _LoginPageState extends State<LoginPage> {
                             style: ElevatedButton.styleFrom(
                               minimumSize: const Size.fromHeight(56),
                             ),
-                            onPressed: () {},
+                            onPressed: () {
+                              switch (_formKey.currentState?.validate()) {
+                                case (false || null):
+                                  Messages.showError(
+                                    'Campos inválidos',
+                                    context,
+                                  );
+                                case true:
+                                  login(_emailEC.text, _passwordEC.text);
+                              }
+                            },
                             child: Text('Acessar'.toUpperCase()),
                           ),
                         ],
