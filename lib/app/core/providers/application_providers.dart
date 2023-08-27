c3377d306cbdd1c4163bf280f37dfd57081f78f1
JohnRocha <jrocha@tractian.com>
2023-08-27 12:30:42 -0300

    Configura o provider do /me

--- lib/app/core/providers/application_providers.dart
+++ lib/app/core/providers/application_providers.dart
@@ -1,4 +1,7 @@
+import 'package:dw_barbershop/app/core/exceptions/repository_exception.dart';
+import 'package:dw_barbershop/app/core/fp/either.dart';
 import 'package:dw_barbershop/app/core/rest_client/rest_client.dart';
+import 'package:dw_barbershop/app/models/user_model.dart';
 import 'package:dw_barbershop/app/repositories/user/user_repository.dart';
 import 'package:dw_barbershop/app/repositories/user/user_repository_impl.dart';
 import 'package:dw_barbershop/app/services/user_login/user_login_service.dart';
@@ -17,3 +20,12 @@ UserRepository userRepository(UserRepositoryRef ref) =>
 @Riverpod(keepAlive: true)
 UserLoginService userLoginService(UserLoginServiceRef ref) =>
     UserLoginServiceImpl(userRepository: ref.read(userRepositoryProvider));
+
+@Riverpod(keepAlive: true)
+Future<UserModel> getMe(GetMeRef ref) async {
+  final result = await ref.watch(userRepositoryProvider).me();
+  return switch (result) {
+    Success(value: final userModel) => userModel,
+    Failure(:final exception) => throw exception,
+  };
+}
