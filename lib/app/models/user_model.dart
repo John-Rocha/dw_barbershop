e70b69515cb203440189684950de295a4fe18d38
JohnRocha <jrocha@tractian.com>
2023-08-27 12:20:10 -0300

    criada a estrutura /me no repositório

--- lib/app/models/user_model.dart
+++ lib/app/models/user_model.dart
@@ -10,6 +10,14 @@ sealed class UserModel {
     required this.email,
     this.avatar,
   });
+
+  factory UserModel.fromMap(Map<String, dynamic> json) {
+    return switch (json['profile']) {
+      'ADM' => UserModelAdm.fromMap(json),
+      'EMPLOYEE' => UserModelEmployee.fromMap(json),
+      _ => throw ArgumentError('User Profile Not Found'),
+    };
+  }
 }
 
 class UserModelAdm extends UserModel {
--- lib/app/repositories/user/user_repository.dart
+++ lib/app/repositories/user/user_repository.dart
@@ -1,6 +1,9 @@
 import 'package:dw_barbershop/app/core/exceptions/auth_exception.dart';
+import 'package:dw_barbershop/app/core/exceptions/repository_exception.dart';
 import 'package:dw_barbershop/app/core/fp/either.dart';
+import 'package:dw_barbershop/app/models/user_model.dart';
 
 abstract interface class UserRepository {
   Future<Either<AuthException, String>> login(String email, String password);
+  Future<Either<RepositoryException, UserModel>> me();
 }
